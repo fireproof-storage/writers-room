4ff7fe2c48e5e1bac89dcd801b97d17cb2f8061a
Chris Anderson <jchris@gmail.com>
2023-09-20 13:59:09 -0700

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.html', './src/**/*.js', './src/**/*.jsx', './src/**/*.ts', './src/**/*.tsx'],
   theme: {
     extend: {},
   },
