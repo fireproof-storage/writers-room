0cf8c9fb0924566a5ebe0029141492c553e9e66e
valorant-dhruv <78591597+valorant-dhruv@users.noreply.github.com>
2023-10-17 21:50:43 +0000

    more changes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from 'react'
 import { useFireproof } from 'use-fireproof'
-import {connect} from '@fireproof/ipfs'
+import { connect } from '@fireproof/ipfs'
 import { Login } from './Login'
 import { Topics } from './Topics'
 
 export function Sidebar() {
-  const {database}  = useFireproof('topics')
+  const { database } = useFireproof('topics')
   const [authorized, setAuthorized] = useState(false)
   const [userEmail, setUserEmail] = useState(localStorage.getItem('user-email') || '')
-  const cx=connect.ipfs(database)
+  const cx = connect.ipfs(database)
 
   useEffect(() => {
     cx.ready.then(() => {
